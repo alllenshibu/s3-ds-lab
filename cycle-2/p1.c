b34cfdc4822ec72fb7dde6d14f51918ce0108d90
JOZEF ANTONY NEELAMKAVIL <67110139+jzf21@users.noreply.github.com>
2022-10-19 09:26:23 +0000

    Update p1.c

--- cycle-2/p1.c
+++ cycle-2/p1.c
@@ -1,81 +1,109 @@
-#include <stdio.h>
+#include<stdio.h>
 
 typedef struct
 {
-    int coeff;
-    int expo;
-} polynomial;
+	int row;
+	int col;
+	int val;
 
-int main()
-{
-    int size1, size2;
-    printf("Enter the size of both polynomials: ");
-    scanf("%d %d", &size1, &size2);
-    polynomial p1[size1], p2[size2], sum[size1 + size2];
+}sparse;
+ #define MAX 100
 
-    printf("Enter the coefficient and power of the polynomial 1 in descending order:\n");
-    printf("For example, 3x^2+2x+1 can be entered as 3 2  2 1  1 0\n");
-    for (int i = 0; i < size1; i++)
-        scanf("%d %d", &p1[i].coeff, &p1[i].expo);
-    printf("Enter the coefficient and power of the polynomial 2 in descending order:\n");
-    printf("For example, 3x^2+2x+1 can be entered as 3 2  2 1  1 0\n");
-    for (int i = 0; i < size2; i++)
-        scanf("%d %d", &p2[i].coeff, &p2[i].expo);
-
-    printf("Polynomial 1\n");
-    for (int i = 0; i < size1; i++)
-        printf("%dx^%d ", p1[i].coeff, p1[i].expo);
-    printf("\n");
-    printf("Polynomial 2\n");
-    for (int i = 0; i < size2; i++)
-        printf("%dx^%d ", p2[i].coeff, p2[i].expo);
-    printf("\n");
-
-    int i = 0, j = 0, z = 0;
-    while (i < size1 && j < size2)
+void sumsparse(sparse s_matrix_1[],sparse  s_matrix_2[],sparse s_matrix_sum[]){
+  if (s_matrix_1[0].row != s_matrix_2[0].row && s_matrix_1[0].col != s_matrix_2[0].col)
     {
-        if (p1[i].expo > p2[j].expo)
-        {
-            sum[z].expo = p1[i].expo;
-            sum[z].coeff = p1[i].coeff;
-            i++;
-            z++;
-        }
-        else if (p1[i].expo < p2[j].expo)
-        {
-            sum[z].expo = p2[j].expo;
-            sum[z].coeff = p2[j].coeff;
-            j++;
-            z++;
-        }
-        else
-        {
-            sum[z].expo = p1[i].expo;
-            sum[z].coeff = p1[i].coeff + p2[j].coeff;
-            z++;
-            i++;
-            j++;
-        }
+        printf("Matrix dimesions are incompatiable\n");
+        
     }
-    while (i < size1)
-    {
-        sum[z].expo = p1[i].expo;
-        sum[z].coeff = p1[i].coeff;
-        i++;
-        z++;
-    }
-    while (j < size2)
-    {
-        sum[z].expo = p2[j].expo;
-        sum[z].coeff = p2[j].coeff;
-        j++;
-        z++;
-    }
+
+    
+
+    s_matrix_sum[0].row = s_matrix_1[0].row;
+    s_matrix_sum[0].col = s_matrix_1[0].col;
+
+    int z = 1, x = 1, y = 1;
+
+    for (int i = 0; i < s_matrix_1[0].row; i++)
+        for (int j = 0; j < s_matrix_1[0].col; j++)
+        {
+            if (s_matrix_1[x].row == i && s_matrix_1[x].col == j && s_matrix_2[y].row == i && s_matrix_2[y].col == j)
+            {
+                s_matrix_sum[z].row = i;
+                s_matrix_sum[z].col = j;
+                s_matrix_sum[z].val = s_matrix_1[x].val+ s_matrix_2[y].val;
+                x++;
+                y++;
+                z++;
+            }
+            else if (s_matrix_1[x].row == i && s_matrix_1[x].col == j)
+            {
+                s_matrix_sum[z].row = i;
+                s_matrix_sum[z].col = j;
+                s_matrix_sum[z].val= s_matrix_1[x].val;
+                x++;
+                z++;
+            }
+            else if (s_matrix_2[y].row == i && s_matrix_2[y].col == j)
+            {
+                s_matrix_sum[z].row = i;
+                s_matrix_sum[z].col = j;
+                s_matrix_sum[z].val = s_matrix_2[y].val;
+                y++;
+                z++;
+            }
+        }
+    s_matrix_sum[0].val = --z;
 
     printf("Sum\n");
-    for (int i = 0; i < z; i++)
-        printf("%dx^%d ", sum[i].coeff, sum[i].expo);
-    printf("\n");
+    for (int i = 0; i <= s_matrix_sum[0].val; i++)
+        printf("%d %d %d\n", s_matrix_sum[i].row, s_matrix_sum[i].col, s_matrix_sum[i].val);
 
-    return 0;
+    
+	
+
+}
+
+ void readsparse(sparse a[], int m, int n)
+{
+            int i, j, k, item, p;
+            a[0].row = m;
+            a[0].col = n;
+            k = 1;
+
+            printf("\nEnter the elements:\n");
+            for(i=0; i<m; i++)
+            {
+                        for(j=0; j<n; j++)
+                        {
+                                    scanf("%d",&item);
+                                    if(item == 0)
+                                                continue;
+                                    a[k].row = i;
+                                    a[k].col = j;
+                                    a[k].val = item;
+                                    k++;
+                        }
+            }
+            a[0].val = k-1;
+
+            printf("\nThe entered sparse matrix is:\n");
+            printf("\nRow\tColumn\tValue\n");
+            for(p=0;  p<=a[0].val;  p++)
+            {
+                        printf("%d\t", a[p].row);
+                        printf("%d\t", a[p].col);
+                        printf("%d\n", a[p].val);
+            }
+}
+
+int main(){
+   int m, n, key;
+            sparse a[MAX], b[MAX],c[MAX];
+            printf("\nEnter  the no of rows and columns:\t");
+            scanf("%d%d",&m, &n);
+
+            readsparse(a, m, n);
+            readsparse(b, m, n);
+            sumsparse(a,b,c);
+          
 }
